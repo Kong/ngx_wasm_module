a8c43cc172a916b5e758d4cb26bf1239c7e66058
Thibault Charbonnier <thibaultcha@me.com>
2021-04-14 23:45:11 -0700

    tests(proxy-wasm) cover get_http_request_header missing header

--- t/lib/rust-tests/proxy-wasm/hostcalls/src/echo.rs
+++ t/lib/rust-tests/proxy-wasm/hostcalls/src/echo.rs
@@ -13,7 +13,12 @@ pub(crate) fn echo_headers(ctx: &mut TestHttpHostcalls) {
 }
 
 pub(crate) fn echo_header(ctx: &mut TestHttpHostcalls, name: &str) {
-    let value = ctx.get_http_request_header(name).unwrap();
-    let body = format!("{}: {}", name, value);
+    let value = ctx.get_http_request_header(name);
+    if value.is_none() {
+        ctx.send_plain_response(StatusCode::OK, Some(format!("{}:", name)));
+        return;
+    }
+
+    let body = format!("{}: {}", name, value.unwrap());
     ctx.send_plain_response(StatusCode::OK, Some(body));
 }
