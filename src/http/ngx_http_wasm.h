d60e30e7b84ab781a518d95eca3a348eedf339a9
Thibault Charbonnier <thibaultcha@me.com>
2021-04-21 20:38:55 -0700

    misc(ngx_http_wasm) split directives into ngx_http_wasm_directives.c

--- src/http/ngx_http_wasm.h
+++ src/http/ngx_http_wasm.h
@@ -41,6 +41,19 @@ typedef struct {
 } ngx_http_wasm_req_ctx_t;
 
 
+typedef struct {
+    ngx_wavm_t                        *vm;
+    ngx_wasm_ops_engine_t             *ops_engine;
+    ngx_proxy_wasm_t                  *pwmodule;
+    ngx_queue_t                        q;
+} ngx_http_wasm_loc_conf_t;
+
+
+typedef struct {
+    ngx_queue_t                        ops_engines;
+} ngx_http_wasm_main_conf_t;
+
+
 /* ops */
 ngx_int_t ngx_http_wasm_stash_local_response(ngx_http_wasm_req_ctx_t *rctx,
     ngx_int_t status, u_char *reason, size_t reason_len, ngx_array_t *headers,
@@ -73,7 +86,15 @@ ngx_http_wasm_resp_headers_count(ngx_http_request_t *r)
 }
 
 
-extern ngx_wavm_host_def_t  ngx_http_wasm_host_interface;
+/* directives */
+char *ngx_http_wasm_call_directive(ngx_conf_t *cf, ngx_command_t *cmd,
+    void *conf);
+char *ngx_http_wasm_proxy_wasm_directive(ngx_conf_t *cf, ngx_command_t *cmd,
+    void *conf);
+
+
+extern ngx_wasm_subsystem_t  ngx_http_wasm_subsystem;
+extern ngx_wavm_host_def_t   ngx_http_wasm_host_interface;
 
 
 #endif /* _NGX_HTTP_WASM_H_INCLUDED_ */
