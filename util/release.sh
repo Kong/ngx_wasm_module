962a75aa858d9c214794094fe89634ca11bf5a70
Thibault Charbonnier <thibaultcha@me.com>
2021-05-24 23:12:40 -0700

    chore(ci) revisit release-nightly.ci action

--- util/release.sh
+++ util/release.sh
@@ -18,8 +18,6 @@ PCRE_VER=8.44
 OPENSSL_VER=1.1.1k
 ZLIB_VER=1.2.11
 
-arch=$(uname -m)
-
 while (( "$#" )); do
     case "$1" in
         --static)
@@ -54,21 +52,21 @@ if [ -z "$name" ]; then
 fi
 
 DIR_DIST_WORK=$DIR_WORK/ngx_wasm_module_dist
-DIST_SRC=ngx_wasm_module-$name
 DIR_BUILD=$DIR_DIST_WORK/build
+DIST_SRC=ngx_wasm_module-$name
 
 mkdir -p $DIR_DIST_WORK $DIR_DIST_OUT
-cd $DIR_DIST_WORK
-
-if [[ -d $DIST_SRC || -d $DIST_BIN ]]; then
-    rm -rf $DIST_SRC $DIST_BIN
-fi
-
-mkdir -p $DIR_DIST_OUT $DIST_SRC $DIST_BIN
 
 # ngx_wasm_module-$name.tar.gz (sources)
 
-echo "Creating source archive..."
+notice "Creating source archive..."
+cd $DIR_DIST_WORK
+
+if [[ -d $DIST_SRC ]]; then
+    rm -rf $DIST_SRC
+fi
+
+mkdir -p $DIST_SRC
 cp -R \
     $NGX_WASM_DIR/config \
     $NGX_WASM_DIR/src \
@@ -76,104 +74,117 @@ cp -R \
 
 tar czf $DIST_SRC.tar.gz $DIST_SRC
 mv $DIST_SRC.tar.gz $DIR_DIST_OUT
-echo "Created $DIR_DIST_OUT/$DIST_SRC.tar.gz"
+notice "Created $DIR_DIST_OUT/$DIST_SRC.tar.gz"
 
 # nginx binary (static)
 
-if [ -z "$RELEASE_STATIC" ]; then
-    exit
+build_static_binary() {
+    local arch=$(uname -m)
+    local runtime=$1
+    local runtime_ver=$2
+
+    case $OSTYPE in
+        linux*)  os="linux" ;;
+        darwin*) os="macos" ;;
+        *)       fatal "unsupported OS \"$OSTYPE\""
+    esac
+
+    local dist_bin_name=wasmx-$name-$runtime-$arch-$os
+
+    if [ -z "$NGX_VER" ]; then
+        fatal "$SCRIPT_NAME missing nginx version for static build, specify --ngx <ver>"
+    fi
+
+    notice "Building statically linked binary $dist_bin_name..."
+    cd $DIR_DIST_WORK
+    mkdir -p $dist_bin_name
+
+    download nginx-$NGX_VER.tar.gz \
+        "https://nginx.org/download/nginx-$NGX_VER.tar.gz"
+
+    if [ ! -d "nginx-$NGX_VER" ]; then
+        tar -xf nginx-$NGX_VER.tar.gz
+    fi
+
+    # openssl
+
+    download openssl-$OPENSSL_VER.tar.gz \
+        "https://www.openssl.org/source/openssl-$OPENSSL_VER.tar.gz"
+
+    if [ ! -d "openssl-$OPENSSL_VER" ]; then
+        tar -xf openssl-$OPENSSL_VER.tar.gz
+    fi
+
+    # pcre
+
+    download pcre-$PCRE_VER.tar.gz \
+        "https://ftp.pcre.org/pub/pcre/pcre-$PCRE_VER.tar.gz"
+
+    if [ ! -d "pcre-$PCRE_VER" ]; then
+        tar -xf pcre-$PCRE_VER.tar.gz
+    fi
+
+    # zlib
+
+    download zlib-$ZLIB_VER.tar.gz \
+        "https://www.zlib.net/zlib-$ZLIB_VER.tar.gz"
+
+    if [ ! -d "zlib-$ZLIB_VER" ]; then
+        tar -xf zlib-$ZLIB_VER.tar.gz
+    fi
+
+    # build
+
+    cd nginx-$NGX_VER
+
+    ./configure \
+        --build="wasmx $name (vm: $NGX_WASM_RUNTIME, nginx: $NGX_VER)" \
+        --builddir=$DIR_BUILD \
+        --with-cc-opt='-g -O3' \
+        --with-ld-opt='-lm -ldl -lpthread' \
+        --add-module="$DIR_DIST_WORK/$DIST_SRC" \
+        --with-openssl="$DIR_DIST_WORK/openssl-$OPENSSL_VER" \
+        --with-zlib="$DIR_DIST_WORK/zlib-$ZLIB_VER" \
+        --with-pcre="$DIR_DIST_WORK/pcre-$PCRE_VER" \
+        --with-pcre-jit
+
+    make -j${n_jobs}
+
+    #if nm -g "$DIR_BUILD/nginx" | grep -qv "T wasm_instance_new"; then
+    #    fatal "$NGX_WASM_RUNTIME lib incorrectly linked"
+    #fi
+
+    cd $DIR_DIST_WORK
+    cp $DIR_BUILD/nginx \
+       $NGX_WASM_DIR/misc/nginx.conf \
+       $NGX_WASM_DIR/misc/README \
+        $dist_bin_name
+
+    tar czf $dist_bin_name.tar.gz $dist_bin_name
+    mv $dist_bin_name.tar.gz $DIR_DIST_OUT
+    notice "Created $DIR_DIST_OUT/$dist_bin_name.tar.gz"
+}
+
+if [ -n "$RELEASE_STATIC" ]; then
+    if [ -n "$WASMTIME_VER" ]; then
+        download_wasmtime $WASMTIME_VER
+
+        export NGX_WASM_RUNTIME=wasmtime
+        export NGX_WASM_RUNTIME_INC="$(pwd)/wasmtime-$WASMTIME_VER/include"
+        export NGX_WASM_RUNTIME_LD_OPT="$(pwd)/wasmtime-$WASMTIME_VER/lib/libwasmtime.a -lm"
+
+        build_static_binary wasmtime $WASMTIME_VER
+    fi
+
+    if [ -n "$WASMER_VER" ]; then
+        download_wasmer $WASMER_VER
+
+        export NGX_WASM_RUNTIME=wasmer
+        export NGX_WASM_RUNTIME_INC="$(pwd)/wasmer-$WASMER_VER/include"
+        export NGX_WASM_RUNTIME_LD_OPT="$(pwd)/wasmer-$WASMER_VER/lib/libwasmer.a -lm"
+
+        build_static_binary wasmer $WASMER_VER
+    fi
 fi
-
-if [ -z "$NGX_VER" ]; then
-    fatal "$SCRIPT_NAME missing --ngx <ver> argument for static build"
-fi
-
-echo "Building statically linked binary..."
-cd $DIR_DIST_WORK
-
-download nginx-$NGX_VER.tar.gz \
-    "https://nginx.org/download/nginx-$NGX_VER.tar.gz"
-
-if [ ! -d "nginx-$NGX_VER" ]; then
-    tar -xf nginx-$NGX_VER.tar.gz
-fi
-
-if [ -n "$WASMTIME_VER" ]; then
-    download_wasmtime $WASMTIME_VER
-
-    export NGX_WASM_RUNTIME=wasmtime
-    export NGX_WASM_RUNTIME_INC="$(pwd)/wasmtime-$WASMTIME_VER/include"
-    export NGX_WASM_RUNTIME_LD_OPT="$(pwd)/wasmtime-$WASMTIME_VER/lib/libwasmtime.a -lm"
-
-elif [ -n "$WASMER_VER" ]; then
-    download_wasmer $WASMER_VER
-
-    export NGX_WASM_RUNTIME=wasmer
-    export NGX_WASM_RUNTIME_INC="$(pwd)/wasmer-$WASMER_VER/include"
-    export NGX_WASM_RUNTIME_LD_OPT="$(pwd)/wasmer-$WASMER_VER/lib/libwasmer.a -lm"
-
-else
-    fatal "$SCRIPT_NAME missing --wasmtime/--wasmer <ver> argument for static build"
-fi
-
-DIST_BIN=wasmx-$name-$NGX_WASM_RUNTIME-$arch
-
-# openssl
-
-download openssl-$OPENSSL_VER.tar.gz \
-    "https://www.openssl.org/source/openssl-$OPENSSL_VER.tar.gz"
-
-if [ ! -d "openssl-$OPENSSL_VER" ]; then
-    tar -xf openssl-$OPENSSL_VER.tar.gz
-fi
-
-# pcre
-
-download pcre-$PCRE_VER.tar.gz \
-    "https://ftp.pcre.org/pub/pcre/pcre-$PCRE_VER.tar.gz"
-
-if [ ! -d "pcre-$PCRE_VER" ]; then
-    tar -xf pcre-$PCRE_VER.tar.gz
-fi
-
-# zlib
-
-download zlib-$ZLIB_VER.tar.gz \
-    "https://www.zlib.net/zlib-$ZLIB_VER.tar.gz"
-
-if [ ! -d "zlib-$ZLIB_VER" ]; then
-    tar -xf zlib-$ZLIB_VER.tar.gz
-fi
-
-# build
-
-cd nginx-$NGX_VER
-
-./configure \
-    --build="wasmx $name" \
-    --builddir=$DIR_BUILD \
-    --with-cc-opt='-O3' \
-    --with-ld-opt='-static-libgcc' \
-    --add-module="$DIR_DIST_WORK/$DIST_SRC" \
-    --with-openssl="$DIR_DIST_WORK/openssl-$OPENSSL_VER" \
-    --with-zlib="$DIR_DIST_WORK/zlib-$ZLIB_VER" \
-    --with-pcre="$DIR_DIST_WORK/pcre-$PCRE_VER" \
-    --with-pcre-jit
-
-make -j${n_jobs}
-
-if nm -g "$DIR_BUILD/nginx" | grep -qv "T wasm_instance_new"; then
-    fatal "$NGX_WASM_RUNTIME lib incorrectly linked"
-fi
-
-cd $DIR_DIST_WORK
-cp $DIR_BUILD/nginx \
-   $NGX_WASM_DIR/misc/nginx.conf \
-   $NGX_WASM_DIR/misc/README \
-    $DIST_BIN
-
-tar czf $DIST_BIN.tar.gz $DIST_BIN
-mv $DIST_BIN.tar.gz $DIR_DIST_OUT
-echo "Created $DIR_DIST_OUT/$DIST_BIN.tar.gz"
 
 # vim: ft=sh st=4 sts=4 sw=4:
