7f6fa5ed3fd9da1e220f6f665aa9bb5f75bd612e
Thibault Charbonnier <thibaultcha@me.com>
2022-04-28 11:53:08 -0700

    chore(release) bump OpenSSL and zlib versions

--- util/release.sh
+++ util/release.sh
@@ -15,8 +15,8 @@ source $NGX_WASM_DIR/util/_lib.sh
 
 ###############################################################################
 PCRE_VER=8.45
-OPENSSL_VER=1.1.1l
-ZLIB_VER=1.2.11
+OPENSSL_VER=1.1.1n
+ZLIB_VER=1.2.12
 
 while (( "$#" )); do
     case "$1" in
