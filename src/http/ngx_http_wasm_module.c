7d6f664ff781b5579e527fa34dbdfda37a9ff1f0
Thibault Charbonnier <thibaultcha@me.com>
2022-06-02 19:58:19 -0700

    fix(http_wasm) proxy_wasm_isolation takes one argument

--- src/http/ngx_http_wasm_module.c
+++ src/http/ngx_http_wasm_module.c
@@ -141,7 +141,7 @@ static ngx_command_t  ngx_http_wasm_module_cmds[] = {
       NULL },
 
     { ngx_string("proxy_wasm_isolation"),
-      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE12,
+      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
       ngx_http_wasm_proxy_wasm_isolation_directive,
       NGX_HTTP_LOC_CONF_OFFSET,
       NGX_HTTP_MODULE,
