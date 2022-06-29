a7325d0b60c09b0cbe4a7b784f5f65e9fcec9a6c
Hisham Muhammad <hisham@gobolinux.org>
2022-06-29 17:30:40 -0300

    feat(ngx-rust) use more general string type
    Use &str instead of String in public API functions for ngx_rust,
    so we can easily use both Strings and string literals.
    
    See:
    https://stackoverflow.com/a/40006220
    https://blog.thoughtram.io/string-vs-str-in-rust/

--- lib/ngx-rust/src/resp.rs
+++ lib/ngx-rust/src/resp.rs
@@ -13,10 +13,10 @@ pub fn ngx_resp_set_status(status: i32) {
     unsafe { ngx_http_resp_set_status(status) }
 }
 
-pub fn ngx_resp_say(body: String) {
+pub fn ngx_resp_say(body: &str) {
     unsafe { ngx_http_resp_say(body.as_ptr(), body.len() as i32) }
 }
 
-pub fn ngx_resp_local_reason(status: i32, reason: String) {
+pub fn ngx_resp_local_reason(status: i32, reason: &str) {
     unsafe { ngx_http_local_response(status, reason.as_ptr(), reason.len() as i32) }
 }
