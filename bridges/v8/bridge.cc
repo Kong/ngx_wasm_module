af0794c1f2bcf5edaa673eb41294fb2ef6868b82
heyang.zhou <heyang.zhou@konghq.com>
2022-07-19 09:17:20 +0000

    fix(v8) run in single-threaded mode and disable liftoff

--- bridges/v8/bridge.cc
+++ bridges/v8/bridge.cc
@@ -2,8 +2,9 @@
 #include "bridge.h"
 
 extern "C" int ngx_v8_enable_wasm_trap_handler(int use_v8_signal_handler) {
-    //v8::V8::SetFlagsFromString("--perf-prof --perf-prof-annotate-wasm --no-liftoff");
-    //v8::V8::SetFlagsFromString("--no-liftoff");
-    //v8::V8::SetFlagsFromString("--liftoff-only");
     return v8::V8::EnableWebAssemblyTrapHandler(use_v8_signal_handler);
+}
+
+extern "C" void ngx_v8_set_flags(const char *flags) {
+    v8::V8::SetFlagsFromString(flags);
 }
