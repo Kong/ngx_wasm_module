30566cb25f93c07faef8f05fc4805c361e5e5b6c
Thibault Charbonnier <thibaultcha@me.com>
2022-07-18 18:43:09 -0700

    fix(ngx_wasi) remove sys/random.h dependency
    Besides the lack of a Rust toolchain, this is the next error in line
    preventing the release scripts from working after the V8 merge.

--- src/wasm/wasi/ngx_wasi_host.c
+++ src/wasm/wasi/ngx_wasi_host.c
@@ -4,28 +4,24 @@
 #include "ddebug.h"
 
 #include <ngx_wavm.h>
-#include <sys/random.h>
 
 
 static ngx_int_t
 ngx_wasi_hfuncs_random_get(ngx_wavm_instance_t *instance,
     wasm_val_t args[], wasm_val_t rets[])
 {
+    size_t     i;
     uint8_t   *buf;
     uint32_t   buf_len;
-    ssize_t    got;
 
-    buf = (uint8_t*) ngx_wavm_memory_lift(instance->memory, args[0].of.i32);
+    buf = (uint8_t *) ngx_wavm_memory_lift(instance->memory, args[0].of.i32);
     buf_len = args[1].of.i32;
 
-    got = getrandom(buf, buf_len, GRND_RANDOM | GRND_NONBLOCK);
-
-    if (got >= 0) {
-        rets[0] = (wasm_val_t) WASM_I32_VAL(0); /* Ok */
-        return NGX_WAVM_OK;
+    for (i = 0; i < buf_len; i++) {
+        buf[i] = (uint8_t) ngx_random();
     }
 
-    rets[0] = (wasm_val_t) WASM_I32_VAL(1); /* Err */
+    rets[0] = (wasm_val_t) WASM_I32_VAL(0); /* Ok */
     return NGX_WAVM_OK;
 }
 
@@ -34,12 +30,9 @@ static ngx_int_t
 ngx_wasi_hfuncs_environ_get(ngx_wavm_instance_t *instance,
     wasm_val_t args[], wasm_val_t rets[])
 {
-    /* TODO nothing is returned for now. */
-
-    /*
-    // here's a sample implementation that documents
+#if 0
+    // A sample implementation documenting
     // the behavior of the arguments:
-
     uint32_t           environ = args[0].of.i32;
     uint32_t           environ_buf = args[1].of.i32;
     uint32_t          *addrs;
@@ -54,7 +47,9 @@ ngx_wasi_hfuncs_environ_get(ngx_wavm_instance_t *instance,
 
     addrs[0] = environ_buf;
     addrs[1] = environ_buf + 6;
-    */
+#endif
+
+    /* TODO: nothing is returned for now */
 
     rets[0] = (wasm_val_t) WASM_I32_VAL(0); /* Ok */
     return NGX_WAVM_OK;
@@ -69,16 +64,18 @@ ngx_wasi_hfuncs_environ_sizes_get(ngx_wavm_instance_t *instance,
     uint32_t          *environ_buf_size;
     ngx_wrt_extern_t  *mem = instance->memory;
 
+    /**
+     * environ_size is the number of environment variables to be returned.
+     * environ_buf_size is the size of the buffer containing the concatenation
+     * of all null-terminated key=value pairs.
+     * For example, given an environment {"A=aaa", "BB=bbbb"},
+     * environ_size would be 2 and environ_buf_size would be 14.
+     */
     environ_size = (uint32_t *) ngx_wavm_memory_lift(mem, args[0].of.i32);
     environ_buf_size = (uint32_t *) ngx_wavm_memory_lift(mem, args[1].of.i32);
 
-    // environ_size is the number of environment variables to be returned.
-    // environ_buf_size is the size of the buffer containing the concatenation
-    // of all null-terminated key=value pairs.
-    // For example, given an environment {"A=aaa", "BB=bbbb"},
-    // environ_size would be 2 and environ_buf_size would be 14.
+    /* TODO: nothing is returned for now */
 
-    // TODO zeros since environ_get returns nothing for now.
     *environ_size = 0;
     *environ_buf_size = 0;
 
