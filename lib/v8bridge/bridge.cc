2f97c18b9a906e1f9b5d9a4a168d178ab15a1400
heyang.zhou <heyang.zhou@konghq.com>
2022-07-21 13:11:14 +0000

    chore(v8) fix and cleanup build and test issues
    Co-authored-by: Hisham Muhammad <hisham@gobolinux.org>
    Co-authored-by: Thibault Charbonnier <thibaultcha@users.noreply.github.com>

--- lib/v8bridge/bridge.cc
+++ lib/v8bridge/bridge.cc
@@ -4,3 +4,7 @@
 extern "C" int ngx_v8_enable_wasm_trap_handler(int use_v8_signal_handler) {
     return v8::V8::EnableWebAssemblyTrapHandler(use_v8_signal_handler);
 }
+
+extern "C" void ngx_v8_set_flags(const char *flags) {
+    v8::V8::SetFlagsFromString(flags);
+}
