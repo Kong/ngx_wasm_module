8324762268f34443c4ae591d0b24017e713d5928
Thibault Charbonnier <thibaultcha@me.com>
2023-02-07 16:57:12 -0800

    chore(release) bump OpenSSL to 1.1.1t

--- util/release.sh
+++ util/release.sh
@@ -15,7 +15,7 @@ source $NGX_WASM_DIR/util/_lib.sh
 
 ###############################################################################
 PCRE_VER=8.45
-OPENSSL_VER=1.1.1s
+OPENSSL_VER=1.1.1t
 ZLIB_VER=1.2.13
 
 while (( "$#" )); do
