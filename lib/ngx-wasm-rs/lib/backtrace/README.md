ea7210901d93ec75e9e85afb51bdf9b6f9c7be41
Hisham Muhammad <hisham@gobolinux.org>
2023-04-06 12:01:51 -0300

    feat(lib) add backtrace module to ngx-wasm-rs

---
+++ lib/ngx-wasm-rs/lib/backtrace/README.md
@@ -0,0 +1,3 @@
+# Backtrace handling module
+
+Operations for turning WASM trap objects into human-readable backtraces.
---
+++ lib/ngx-wasm-rs/lib/backtrace/src/backtrace.rs
@@ -0,0 +1,53 @@
+use rustc_demangle;
+use std::convert::TryInto;
+use wasmparser;
+
+// see specification at:
+// https://webassembly.github.io/spec/core/appendix/custom.html#name-section
+fn read_name_section(
+    mut reader: wasmparser::BinaryReader,
+) -> Result<Vec<(u32, String)>, wasmparser::BinaryReaderError> {
+    let mut table: Vec<(u32, String)> = Vec::new();
+
+    while !reader.eof() {
+        let id = reader.read_u8()?;
+        let size = reader.read_var_u32()?;
+
+        if id != 1 {
+            reader.read_bytes(size.try_into().unwrap())?;
+            continue;
+        }
+
+        let len = reader.read_var_u32()?;
+
+        for _ in 0..len {
+            let idx = reader.read_var_u32()?;
+            let name = reader.read_string()?;
+
+            table.push((idx, name.to_string()));
+        }
+    }
+
+    Ok(table)
+}
+
+pub(crate) fn get_function_name_table(
+    wasm_slice: &[u8],
+) -> Result<Vec<(u32, String)>, wasmparser::BinaryReaderError> {
+    for payload in wasmparser::Parser::new(0).parse_all(wasm_slice) {
+        match payload {
+            Ok(wasmparser::Payload::CustomSection(s)) => {
+                if s.name() == "name" {
+                    let reader = wasmparser::BinaryReader::new(s.data());
+                    return read_name_section(reader);
+                }
+            }
+            _ => {}
+        }
+    }
+    return Ok(Vec::new());
+}
+
+pub(crate) fn demangle(mangled: &str) -> String {
+    rustc_demangle::demangle(mangled).to_string()
+}
