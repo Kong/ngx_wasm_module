c991f8559c36705db43a1afdb791f9094f58f3cb
Hisham Muhammad <hisham@gobolinux.org>
2023-11-22 17:53:03 -0300

    tests(*): match_error_log block and match_error_log_grep modifier
    A new block handler, `match_error_log` where you give it an array of plain
    substrings (not regexes), and the test passes if and only if all occurrences
    of the given substrings in the error log match the given list in the exact
    order, including repeats.
    
    The subset of relevant lines to be checked can be filtered by a regex using
    `match_error_log_grep`. If not given, the entire error.log is checked.

--- t/TestWasm.pm
+++ t/TestWasm.pm
@@ -2,7 +2,7 @@ package t::TestWasm;
 
 use strict;
 use Test::Nginx::Socket::Lua -Base;
-use Test::Nginx::Util qw(gen_rand_port);
+use Test::Nginx::Util qw(gen_rand_port $RunTestHelper);
 use List::Util qw(max);
 use Cwd qw(cwd);
 use Config;
@@ -33,6 +33,10 @@ our @EXPORT = qw(
     skip_valgrind
 );
 
+our $TestNginxRunTestHelper = $RunTestHelper;
+
+$RunTestHelper = \&run_test_helper;
+
 $ENV{TEST_NGINX_USE_HUP} ||= 0;
 $ENV{TEST_NGINX_USE_VALGRIND} ||= 0;
 $ENV{TEST_NGINX_USE_VALGRIND_ALL} ||= 0;
@@ -41,6 +45,13 @@ $ENV{TEST_NGINX_DATA_DIR} = "$pwd/t/data";
 $ENV{TEST_NGINX_CRATES_DIR} = $crates;
 $ENV{TEST_NGINX_UNIX_SOCKET} = html_dir() . "/nginx.sock";
 $ENV{TEST_NGINX_SERVER_PORT2} = gen_rand_port(1000);
+
+my %colors = (
+    UNEXPECTED => "\033[1;31m",
+    ERROR => "\033[1;33;41m",
+    FOUND => "\033[32m",
+    MATCH => "\033[1;41m"
+);
 
 sub skip_valgrind (@) {
     my ($skip) = @_;
@@ -253,6 +264,195 @@ add_block_preprocessor(sub {
         $block->set_value("timeout", $timeout);
     }
 });
+
+sub error_log_data () {
+    open my $in, $Test::Nginx::Util::ErrLogFile or
+        return undef;
+
+    my @lines = <$in>;
+
+    close $in;
+    return \@lines;
+}
+
+sub push_err($$$) {
+    my ($err_out, $str, $color) = @_;
+
+    if ($color ne "") {
+        push(@$err_out, $color);
+    }
+    push(@$err_out, $str);
+    if ($color ne "") {
+        push(@$err_out, "\033[0m");
+    }
+}
+
+sub run_test_helper ($$$) {
+    my ($block, $dry_run, $repeated_req_idx) = @_;
+    my $name = $block->name;
+
+    $TestNginxRunTestHelper->(@_);
+
+    if (defined $block->match_error_log) {
+        my $match_error_log = $block->match_error_log;
+
+        if (ref $match_error_log
+            && ref $match_error_log eq 'ARRAY'
+            && ref $match_error_log->[0]
+            && ref $match_error_log->[0] eq 'ARRAY'
+        ) {
+            $match_error_log = $match_error_log->[$repeated_req_idx];
+        }
+
+        my @substrings;
+        if (ref $match_error_log eq 'ARRAY') {
+            @substrings = @$match_error_log;
+        } else {
+            @substrings = split("\n", $match_error_log);
+        }
+        my $numSubstrings = @substrings;
+
+        my $match_pat;
+        my $match_pats = $block->match_error_log_grep;
+        if (!defined $match_pats) {
+            bail_out("$name - No --- match_error_log_grep defined but --- match_error_log is defined");
+        }
+
+        if (ref $match_pats && ref $match_pats eq 'ARRAY') {
+            $match_pat = $match_pats->[$repeated_req_idx];
+
+        } else {
+            $match_pat = $match_pats;
+        }
+
+        SKIP: {
+            skip "$name - error_log - tests skipped due to $dry_run", 1 if $dry_run;
+
+            my $lines;
+
+            $lines ||= error_log_data();
+
+            my @err_out = ();
+
+            my $i = 0;
+            my $failed = 0;
+            my $newline = 1;
+            my @missing = ();
+            for my $line (@$lines) {
+                if ($newline == 0 ||
+                    (ref $match_pat &&
+                     $line =~ /$match_pat/ ||
+                     $line =~ /\Q$match_pat\E/)
+                ) {
+                    if ($i >= $numSubstrings) {
+                        push_err(\@err_out, $line, "");
+                        if ($newline == 1) {
+                            push_err(\@err_out, "^---- no match declared for the line above.", $colors{"ERROR"});
+                            push_err(\@err_out, "\n", "");
+                        }
+                        $newline = 1;
+                        next;
+                    }
+
+                    my $expected_at = index($line, $substrings[$i]);
+
+                    my $earliest_i = -1;
+                    my $earliest_at = 2**32;
+                    for (my $j = 0; $j < $numSubstrings; $j++) {
+                        my $at = index($line, $substrings[$j]);
+                        if ($at != -1) {
+                            if ($at < $earliest_at) {
+                                $earliest_at = $at;
+                                $earliest_i = $j;
+                            }
+                        }
+                    }
+
+
+                    if ($earliest_i != -1) {
+                        my $earliest_len = length($substrings[$earliest_i]);
+                        my $expected_len = length($substrings[$i]);
+
+                        if ($expected_at != -1 && $earliest_at + $earliest_len >= $expected_at) {
+                            $earliest_i = $i;
+                            $earliest_len = $expected_len;
+                        }
+
+                        my $rest_at = $earliest_at + $earliest_len;
+
+                        push_err(\@err_out, substr($line, 0, $earliest_at), "");
+                        my $color;
+                        if ($earliest_i == $i) {
+                            $color = $colors{"FOUND"};
+                            $i++;
+                        } else {
+                            $color = $colors{"UNEXPECTED"};
+                            $failed = 1;
+                            push(@missing, $substrings[$i]);
+                            $i++;
+                        }
+                        push_err(\@err_out, substr($line, $earliest_at, $earliest_len), $color);
+
+                        $line = substr($line, $rest_at);
+                        if (length($line) > 0) {
+                            $newline = 0;
+                            redo;
+                        }
+
+                    } else {
+                        if ($newline) {
+                            $failed = 1;
+
+                            push(@missing, $substrings[$i]);
+                            $i++;
+                        }
+                    }
+
+                    push_err(\@err_out, $line, "");
+                    for my $p (@missing) {
+                        push_err(\@err_out, "^---- expected ", $colors{"ERROR"});
+                        push_err(\@err_out, $p, $colors{"MATCH"});
+                        push_err(\@err_out, " in line above.", $colors{"ERROR"});
+                        push_err(\@err_out, "\n", "");
+                    }
+                    @missing = ();
+                    $newline = 1;
+                }
+            }
+
+            # fail if we didn't find them all
+            if ($i < $numSubstrings) {
+                $failed = 1;
+                for (my $j = $i; $j < $numSubstrings; $j++) {
+                    push_err(\@err_out, "---- missing a match for ", $colors{"ERROR"});
+                    push_err(\@err_out, $substrings[$j], $colors{"MATCH"});
+                    push_err(\@err_out, "\n", "");
+                }
+            }
+
+            if ($failed == 1) {
+                fail("$name - match_error_log:\n" . join("", @err_out));
+            } else {
+                pass("$name - all substrings were matched in order in error.log");
+            }
+
+            # fail if we didn't find them all
+            if ($i < $numSubstrings) {
+
+                my $ord = ordinate($i);
+                my $missing = $substrings[$i];
+                SKIP: {
+                    skip "$name - error_log - tests skipped due to $dry_run", 1 if $dry_run;
+                }
+            } else {
+            }
+        }
+    } else {
+        if (defined $block->match_error_log_grep) {
+            bail_out("$name - No --- match_error_log defined but --- match_error_log_grep is defined");
+        }
+    }
+}
 
 no_long_string();
 
