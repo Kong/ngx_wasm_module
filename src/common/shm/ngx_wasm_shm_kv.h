a82a51a8154b20447f0c3d227f499a0a9841c2d5
Caio Ramos Casimiro <caiorcasimiro@gmail.com>
2024-04-02 16:48:53 +0100

    refactor(shm/kv) allow retrieving items by key hash
    The change is motivated by the upcoming Proxy-Wasm metrics support,
    which will be built atop `ngx_wasm_shm_kv`.
    
    Proxy-Wasm filter developers are expected to define metrics before using
    them.  When a metric is defined the host returns an id to the filter
    which is later used for updating or retrieving that metric's value.
    
    As such, now `ngx_wasm_shm_kv_get_locked` expects either a key
    (`ngx_str_t *`) or its hash (`uint32_t *`). If both are provided, the
    hash is used and the key is ignored.
    
    A new function `ngx_wasm_shm_rbtree_lookup` is also introduced allowing
    item retrieval using only the key hash.

--- src/common/shm/ngx_wasm_shm_kv.h
+++ src/common/shm/ngx_wasm_shm_kv.h
@@ -15,7 +15,7 @@ typedef struct {
 
 ngx_int_t ngx_wasm_shm_kv_init(ngx_wasm_shm_t *shm);
 ngx_int_t ngx_wasm_shm_kv_get_locked(ngx_wasm_shm_t *shm,
-    ngx_str_t *key, ngx_str_t **value_out, uint32_t *cas);
+    ngx_str_t *key, uint32_t *key_hash, ngx_str_t **value_out, uint32_t *cas);
 ngx_int_t ngx_wasm_shm_kv_set_locked(ngx_wasm_shm_t *shm,
     ngx_str_t *key, ngx_str_t *value, uint32_t cas, ngx_int_t *written);
 ngx_int_t ngx_wasm_shm_kv_resolve_key(ngx_str_t *key,
