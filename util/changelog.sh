a1451799aeade09f8df492604a6b95bf4ff17adf
Thibault Charbonnier <thibaultcha@me.com>
2024-06-24 18:05:04 -0700

    chore(util) new 'changelog' script and Makefile target
    E.g:
    
        $ make changelog
        misc(*) fix log format specifier of 'ssize_t' arguments
        feat(proxy-wasm) implement initial metrics facilities
        refactor(shm/kv) allow retrieving items by key hash
        fix(proxy-wasm) resume content phase after request_body read EAGAIN
        refactor(lua-bridge) remove entry thread sleep timer handling
        fix(proxy-wasm) cancel dispatches when immediately producing a response
        feat(proxy-wasm) dispatch errors do not interrupt filter chains
        refactor(lua-bridge) rewrite for full yielding support
        fix(lib) prevent a segfault loading an empty .wat module with V8
        feat(wasmtime) new 'cache_config' directive for compilation cache
        feat(proxy-wasm) consistent context checks for all header setters
        feat(proxy-wasm) allow setting ':status' response pseudo-header
        refactor(wasm) fix conflicting values in our '*_CONF' definitions
        feat(http) implement 'postpone_rewrite' + 'postpone_access' directives
        refactor(wasmx) store ngx_wa_conf_t as our core module context
        fix(ffi) allow 'load()' to fail and be invoked again
        fix(shm/kv) ensure memory realloc when value is replaced by a larger one
        feat(*) implement ipc modules & generalize WasmX namespace
    
    Changelog author can paste the list in CHANGELOG.md and
    filter-out/format the last bits.

---
+++ util/changelog.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+
+SCRIPT_NAME=$(basename $0)
+NGX_WASM_DIR=${NGX_WASM_DIR:-"$(
+    cd $(dirname $(dirname ${0}))
+    pwd -P
+)"}
+if [[ ! -f "${NGX_WASM_DIR}/util/_lib.sh" ]]; then
+    echo "Unable to source util/_lib.sh" >&2
+    exit 1
+fi
+
+source $NGX_WASM_DIR/util/_lib.sh
+
+###############################################################################
+
+cd $NGX_WASM_DIR
+
+# TODO: be smarter about grabbing the proper tag once we have more than just
+# prerelease tags
+LATEST_TAG=$(git describe --tags --abbrev=0)
+
+# Only show user-facing commits. Include refactor(), misc() and use best
+# judgement whether or not to include them in the final release description).
+git --no-pager log --format=oneline $LATEST_TAG.. \ | sed "s/^[^ ]* //" \ |
+    $(which grep) -v -E '^(chore|tests|style|hotfix|docs)'
