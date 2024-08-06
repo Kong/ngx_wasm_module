9559a36ae0dccced556d5f128264659310c764f8
Hisham Muhammad <hisham@gobolinux.org>
2024-08-06 12:03:30 -0300

    perf(http) stash local response in request pool
    This is to avoid mounting memory pressure as a longer-lived
    connection gets reused across many requests.

--- src/http/ngx_http_wasm_local_response.c
+++ src/http/ngx_http_wasm_local_response.c
@@ -150,7 +150,7 @@ ngx_http_wasm_stash_local_response(ngx_http_wasm_req_ctx_t *rctx,
             b->sync = 1;
         }
 
-        cl = ngx_wasm_chain_get_free_buf(r->connection->pool,
+        cl = ngx_wasm_chain_get_free_buf(rctx->pool,
                                          &rctx->free_bufs, len,
                                          buf_tag, 1);
         if (cl == NULL) {
